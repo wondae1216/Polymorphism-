26d27052f93c5a36e663397ad6b263420a28ed4b
wondae1216 <rkddnjsekl@naver.com>
2024-10-24 17:50:24 +0900

    Add files via upload

---
+++ main/java/Mario/Mario.java
@@ -0,0 +1,23 @@
+package Mario;
+
+public class Mario {
+
+        private boolean journey;
+
+    public Mario() {
+
+        System.out.println("마리오가 피치 공주를 구하러 모험을 나선다");
+
+    }
+
+    public void run() {
+
+        journey= true;
+        System.out.println("마리오가 모헙을 떠납니다.");
+    }
+
+    public void Monster() {
+
+        System.out.println("마리오가 모험 도중 굼바를 만났다.");
+    }
+}
